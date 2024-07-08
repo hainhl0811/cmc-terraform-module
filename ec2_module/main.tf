8d16a24fb8589c66bf3b5a4da251fb1e92c88ff9
hainhl0811 <126540115+hainhl0811@users.noreply.github.com>
2024-07-08 09:06:11 +0700

    Update main.tf

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -8,8 +8,8 @@ data "cmccloudv2_subnet" "rke_subnet"{
 }
 # define flavor
 data "cmccloudv2_flavor_ec" "flavor_ec"{
-    cpu = var.cpu # chọn thông số vCPU
-    ram = var.ram  #chọn thông số RAM
+    cpu = var.flavor_cpu # chọn thông số vCPU
+    ram = var.flavor_ram  #chọn thông số RAM
 }
 # define image
 data "cmccloudv2_image" "ubuntu2204" {
@@ -18,32 +18,27 @@ data "cmccloudv2_image" "ubuntu2204" {
     name = "22.04"  # search image with name contains `22.04`
 }
 
-locals {
-   test_flavor_id = "${data.cmccloudv2_flavor_ec.flavor_ec.id}"
-   test_subnet_id = "${data.cmccloudv2_subnet.rke_subnet.id}"
-}
-
 resource "cmccloudv2_server" "terraform_provision_node" {
-    name = var.name 
-    billing_mode = var.billing_mode 
-    zone = var.zone
-    subnet_id = local.test_subnet_id
-    ip_address = var.ip_address
-    flavor_id = local.test_flavor_id
+    name = var.ecs_name 
+    billing_mode = var.ecs_billing_mode 
+    zone = var.ecs_zone
+    ip_address = var.ecs_ip_address
+    subnet_id = "${data.cmccloudv2_subnet.rke_subnet.id}"
+    flavor_id = "${data.cmccloudv2_flavor_ec.flavor_ec.id}"
     source_id = "${data.cmccloudv2_image.ubuntu2204.id}"
-    source_type = var.source_type
-    volume_size = var.volume_size
-    volume_type = var.volume_type
-    security_group_names = var.security_group_names
-    key_name = var.key_name
-    password = var.password
+    source_type = var.ecs_source_type
+    volume_size = var.ev_size
+    volume_type = var.ev_type
+    security_group_names = var.sg_names
+    key_name = var.ecs_key_name
+    password = var.ecs_password
     vm_state = "active"
 
     connection {
     type     = "ssh"
     user     = "root"
-    password = var.password
-    host     = var.ip_address
+    password = var.ecs_password
+    host     = var.ecs_ip_address
     }
 
     provisioner "remote-exec"{
